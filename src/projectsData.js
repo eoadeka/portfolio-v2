deadec31b01189107ba1acf6bfec37aeeda3bc69
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-01-14 23:31:37 +0000

    feat: Added json list of projects

---
+++ src/projectsData.js
@@ -0,0 +1,50 @@
+import img1 from "./images/img_1.jpg";
+import img2 from "./images/img_2.jpg";
+import img3 from "./images/img_3.jpg";
+
+const data =  
+[
+    {
+        "id": 1,
+        "title": "Optimised CI/CD Pipeline",
+        "img": img1,
+        "description": "A CI/CD Pipeline optimised CI/CD Pipeline optimised",
+        "tools": [
+            "aws",
+            "python",
+            "bash",
+            "terraform",
+            "jenkins",
+            "docker",
+            "git",
+            "github",
+            "prometheus",
+            "grafana"
+        ],
+        "url": "optimised-ci-cd-pipeline"
+    },
+    {
+        "id": 2,
+        "title": "Jectam",
+        "img": img2,
+        "description": "An integrated automated project management system",
+        "tools": [
+            "aws",
+            "terraform"
+        ],
+        "url": "jectam"
+    },
+    {
+        "id": 3,
+        "title": "DOPI",
+        "img": img3,
+        "description": "A curated list of DevOps Project Ideas in one website",
+        "tools": [
+            "aws",
+            "terraform"
+        ],
+        "url": "dopi"
+    }
+]
+
+export default data;
