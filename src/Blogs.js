20c1c69168eaaf5f9fc314716b1a0e20d85dc841
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-01-14 23:34:14 +0000

    feat: Add modal window for project description

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,38 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
-const HasnodeArticles = () => {
-  const [articles, setArticles] = useState([]);
 
- const Secret_KEY = process.env.MY_DEV_KEY;
+const Blog = () => {
+    const [posts, setPosts] = useState([]);
+    const style = { fontSize: "1em", verticalAlign: "middle" };
 
- useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.hashnode.com/v1/me/articles',  {mode:'cors'}, {
+    // const Secret_KEY = process.env.DEV_KEY
+
+    const query = `
+        query {
+            user(username: "ellaadeka") {
+            publication {
+                posts(page:0){
+                slug
+                title
+                brief
+                coverImage
+                cuid
+                _id
+                }
+            }
+            }
+        }
+        `; //as used in the playground
+
+
+    useEffect(() => {
+        fetchPosts();
+    }, []); //we are maintaining side effects here. since we are making an API call in react
+
+
+    const fetchPosts = async () => {
+
+        const response = await fetch("https://api.hashnode.com", {
+          method: "POST",
           headers: {
-            Authorization: Secret_KEY
-          }
+            "Content-type": "application/json",
+            // "Authorization": "Bearer ${Secret_KEY}", //if you're using token
+          },
+          body: JSON.stringify({ query }),
         });
-        const data = await response.json();
-        setArticles(data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
 
-  return (
-    <div>
-      {articles.map(article => (
-        <div key={article._id}>
-          <h2>{article.title}</h2>
-          <p>{article.description}</p>
- <p><a href={article.url}>Read more</a> </p>
+        const data = await response.json();
+        // console.log(data);
+        setPosts(data.data.user.publication.posts);
+        // console.log(data.data.user.publication.posts);
+    };
+
+    return (
+        <div >
+            {posts.slice(0,4).map((post) => (
+                <div className='card' key={post.id}>
+                    <a className='card-link' href={`https://ellaadeka.hashnode.dev//${post.slug}`} target="_blank" rel="noreferrer">
+                        <picture>
+                            <img  src={post.coverImage} alt={post.title} width="250px" height="150px"></img>
+                        </picture>
+                        <div className='card-content'>
+                            <h3>{post.title}</h3>
+                            <br></br>
+                            <br></br>
+                            <p className='card-span'><span>read more</span> <span><FaArrowRight style={style} /></span></p>
+                        </div>
+                    </a>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
-export default HasnodeArticles;
+
+export default Blog;
