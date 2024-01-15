d1fd7c607f7243c4468ce8642dcd7eb1b99b265b
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-01-15 22:54:53 +0000

    feat: Add links and coming soon boolean

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,14 +1,17 @@
 import img1 from "./images/img_1.jpg";
 import img2 from "./images/img_2.jpg";
 import img3 from "./images/img_3.jpg";
+import cicd from "./images/ci_cd.png";
 
 const data =  
 [
     {
         "id": 1,
         "title": "Optimised CI/CD Pipeline",
-        "img": img1,
-        "description": "A CI/CD Pipeline optimised CI/CD Pipeline optimised",
+        "thumbnail": img1,
+        "img": cicd,
+        "liveDemo": img1,
+        "description": "A CI/CD Pipeline optimised with IaC and automation",
         "tools": [
             "aws",
             "python",
@@ -19,31 +22,47 @@ const data =
             "git",
             "github",
             "prometheus",
-            "grafana"
+            "grafana",
         ],
-        "url": "optimised-ci-cd-pipeline"
+        "url": "optimised-ci-cd-pipeline",
+        "blogLink": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance",
+        "githubLink": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "liveSite": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "comingSoon": false
     },
     {
         "id": 2,
         "title": "Jectam",
+        "thumbnail": img2,
         "img": img2,
+        "liveDemo": img2,
         "description": "An integrated automated project management system",
         "tools": [
             "aws",
             "terraform"
         ],
-        "url": "jectam"
+        "url": "jectam",
+        "blogLink": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance",
+        "githubLink": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "liveSite": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "comingSoon": true
     },
     {
         "id": 3,
         "title": "DOPI",
+        "thumbnail": img3,
         "img": img3,
+        "liveDemo": img3,
         "description": "A curated list of DevOps Project Ideas in one website",
         "tools": [
             "aws",
             "terraform"
         ],
-        "url": "dopi"
+        "url": "dopi",
+        "blogLink": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance",
+        "githubLink": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "liveSite": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "comingSoon": true
     }
 ]
 
