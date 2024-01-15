f89e72748a64752f6c2b117c1b0dafd90fbcb1f4
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-01-15 22:56:02 +0000

    feat: Add coming soon overlay to upcoming projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { FaArrowRight } from 'react-icons/fa';
-import { MdOutlineCancel } from "react-icons/md";
+// import { FaArrowRight } from 'react-icons/fa';
+import { MdOutlineCancel, MdArrowOutward } from "react-icons/md";
 
 const Card = (props) => {
     const {data} = props;
@@ -26,8 +26,9 @@ const Card = (props) => {
               <div className='card' key={project.id} data-id={project.id}>
                   {/* <a className='card-link' href={project.url}> */}
                   <span className='card-link' id={project.id} onClick={() => handleClick(item)}>
-                      <picture>
-                          <img src={project.img} alt={project.title} width="250px" height="150px"></img>
+                      <picture className='project-pict'>
+                          <img className='project-image' src={project.thumbnail} alt={project.title} width="250px" height="150px"></img>
+                          {project.comingSoon === true && <div className="coming-soon-overlay">coming soon</div>}
                       </picture>
                       <div className='card-content'>
                           <h3>{project.title}</h3>
@@ -35,7 +36,7 @@ const Card = (props) => {
                           <p>{project.tools.slice(0,3).map((tool) => (
                             tool
                           )).join(", ")}</p>
-                          <p className='card-span'><span>view</span> <span><FaArrowRight style={style} /></span></p>
+                          <p className='card-span'><span>view</span> <span><MdArrowOutward style={style} /></span></p>
                       </div>
                   </span>
                   {open && item===selected && (
@@ -43,12 +44,12 @@ const Card = (props) => {
                         <MdOutlineCancel size="2em" onClick={handleClick} style={{position: "absolute", top: "2em", right: "2em"}} />
                         <div key={project.id}  className='overlay-content'>
                             {/* MAKE THIS OVERLAY DISAPPEAR!!! */}
-                            <h2 style={{textAlign: "center"}}>{project.title}</h2>
+                            <h2 style={{textAlign: "center"}}>{project.title}{project.comingSoon === true && <sup style={{fontSize: "15px", background: "#28104e",color: "lavender", borderRadius: "10px", padding:"5px"}}>coming soon</sup>}</h2>
 
                             <div className='contents'>
                                 <div className='project project-picture'>
                                     <picture>
-                                        <img className='project-img' src={project.img} alt={project.title} width="450px" height="400px" style={{filter:"grayscale(0%)"}}></img>
+                                        <img className='project-img' src={project.img} alt={project.title} width="450px" height="420px" style={{filter:"grayscale(0%)", objectFit: "fit", objectPosition: "top"}}></img>
                                     </picture>
                                 </div>
                                 <div className='project project-tools'>
@@ -62,14 +63,14 @@ const Card = (props) => {
                                 <div className='project project-links'>
                                     {/* <p>Links</p> */}
                                     <h4>{project.description}.</h4>
-                                    <p>read my blog on the project</p>
-                                    <p>view on github</p>
-                                    <p>view live site</p>
+                                    <a href={project.blogLink} target="_blank" rel="noreferrer"><MdArrowOutward style={style} /> read my blog on the project</a><br></br>
+                                    <a href={project.githubLink} target="_blank" rel="noreferrer"><MdArrowOutward style={style} /> view code on github</a><br></br>
+                                    <a href={project.liveSite} target="_blank" rel="noreferrer"><MdArrowOutward style={style} /> view live site</a>
                                 </div>
                                 <div className='project project-live-demo'>
                                     {/* <p>Live demo</p> */}
                                     <picture>
-                                        <img src={project.img} alt={project.title} width="685px" height="400px" style={{filter:"grayscale(0%)"}}></img>
+                                        <img src={project.liveDemo} alt={project.title} width="685px" height="430px" style={{filter:"grayscale(0%)"}}></img>
                                     </picture>
                                 </div>
 
