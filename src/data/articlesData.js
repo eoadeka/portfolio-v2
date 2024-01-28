bd6075cde6182ced914d335554944d9db6000cc5
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-01-28 23:15:28 +0000

    feat: Add directory for project data

---
+++ src/data/articlesData.js
@@ -0,0 +1,134 @@
+const posts = [
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1705742715347/dc4845d7-0f6a-44f4-b979-2828a06432aa.png",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Mass Emailing with Lambda",
+        "brief": "Introduction\nOrchestrating and automating mass emailing is crucial for businesses and organisations. In this article, we'' explore a robust solution using AWS Lambda, Simple Email Service (SES), Identity and Access Management (IAM), and CloudWatch. W...",
+        "url": "https://ellaadeka.hashnode.dev/mass-emailing-with-lambda"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1705244403955/0266d710-9831-4dd4-839f-86d925565aa5.jpeg",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Deploying a Flask Application from GitHub on an EC2 Instance",
+        "brief": "Introduction\nAmazon Elastic Compute Cloud (Amazon EC2) stands as a extensively utilized cloud computing service, offering scalable virtual servers within the cloud. If you've crafted a Flask web application and seek a dependable and scalable hosting ...",
+        "url": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1705087924610/3efb6f0a-6b4f-45f5-b743-a96d16c99b03.png",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Step-by-Step Guide on Connecting to EC2 Instances through SSH Client",
+        "brief": "Introduction\nAmazon EC2 instances are virtual servers in the cloud. Think of EC2 instances as renting books from the library, rather than buying books and storing them at home, you could just rent what you need, when you need it. Connecting to EC2 in...",
+        "url": "https://ellaadeka.hashnode.dev/step-by-step-guide-on-connecting-to-ec2-instances-through-ssh-client"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1697677998164/050fd00f-1d0f-41fe-bdd3-c62f60d67d23.jpeg",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Creating an EC2 Instance with Terraform",
+        "brief": "Introduction\nIn the recent times in the technological ecosystem, Infrastructure as Code (IaC) tools have become highly sought after due to their ability to automate the management of cloud resources effectively and efficiently.Terraform, a commonly e...",
+        "url": "https://ellaadeka.hashnode.dev/creating-an-ec2-instance-with-terraform"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1705244441139/e19f2d63-8b70-4e65-b414-406d09692910.jpeg",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Simplifying the Cloud: AWS Services as Analogies",
+        "brief": "Introduction\nCloud services have become the backbone of modern computing. While the concepts of cloud computing can seem complex at first, analogies really help with understanding them, by demystifying these concepts through a relatable lens. In this...",
+        "url": "https://ellaadeka.hashnode.dev/simplifying-the-cloud-aws-services-as-analogies"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1699810521168/6e16e3a2-1478-412f-9e7b-9212f54916e2.png",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Mastering the Craft: The Definitive Guide to Writing Git Commit Messages",
+        "brief": "Introduction\nWelcome to the realm of software development, where lines of code evolve and intertwine to create incredible projects. In this world, Git commit messages take on the crucial role of annotations that narrate the story behind the code. But...",
+        "url": "https://ellaadeka.hashnode.dev/mastering-the-craft-the-definitive-guide-to-writing-git-commit-messages"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1699700773093/49f39fcc-9fe5-4c37-8b0d-a3ccbd179e34.png",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Navigating the Dockerfile commands",
+        "brief": "In the dynamic landscape of modern software development, containerization has emerged as a game-changing technology. At the heart of containerization lies the Dockerfile—a blueprint that defines how Docker images are built. This blog post is your com...",
+        "url": "https://ellaadeka.hashnode.dev/navigating-the-dockerfile-commands"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/stock/unsplash/wX2L8L-fGeA/upload/4f22e8a18765aee05430d16155d948da.jpeg",
+            "attribution": "https://unsplash.com/@synkevych",
+            "photographer": "Roman Synkevych",
+            "isAttributionHidden": false
+        },
+        "title": "Make your first Pull Request(PR) on GitHub",
+        "brief": "In the vast world of open-source software development, contributing to projects on GitHub is a rewarding endeavour that fosters collaboration, skill-building, and community engagement. If you're eager to make your first pull request but don't know wh...",
+        "url": "https://ellaadeka.hashnode.dev/make-your-first-pull-requestpr-on-github"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1697363407291/7ac99516-bfc0-4a68-8614-804415be032c.png",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Understanding EC2 Instance States",
+        "brief": "Introduction\nIn this blog article, we'll deep dive into Amazon Web Services (AWS) EC2 instance states. EC2 instances are virtual servers in the cloud that perform tasks including hosting and running applications. To effectively manage instances, it i...",
+        "url": "https://ellaadeka.hashnode.dev/understanding-ec2-instance-states"
+        }
+    },
+    {
+        "node": {
+        "coverImage": {
+            "url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1697329649285/a51a1735-5a53-407e-bf5b-df159d61f962.png",
+            "attribution": null,
+            "photographer": null,
+            "isAttributionHidden": false
+        },
+        "title": "Terminating EC2 Instances the Right Way",
+        "brief": "Introduction\nIn this article, we discuss why you should terminate your instances and how you can terminate them properly to avoid any lingering configurations that may incur charges on your AWS account. It is essential to terminate your EC2 instances...",
+        "url": "https://ellaadeka.hashnode.dev/terminating-ec2-instances-the-right-way"
+        }
+    }
+]
+
+export default posts;
---
+++ src/data/projectsData.js
@@ -0,0 +1,113 @@
+import img1 from "../images/img_1.jpg";
+import img2 from "../images/img_2.jpg";
+import img3 from "../images/img_3.jpg";
+import cicd from "../images/ci_cd.png";
+
+const data =  
+[
+    {
+        "id": 1,
+        "title": "Optimised CI/CD Pipeline",
+        "thumbnail": img1,
+        "img": cicd,
+        "liveDemo": img1,
+        "description": "A CI/CD Pipeline optimised with IaC and automation",
+        "tools": [
+            "aws",
+            "python",
+            "bash",
+            "terraform",
+            "jenkins",
+            "docker",
+            "git",
+            "github",
+            "prometheus",
+            "grafana",
+        ],
+        "url": "optimised-ci-cd-pipeline",
+        "blogLink": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance",
+        "githubLink": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "liveSite": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+        "comingSoon": false
+    },
+    {
+        "id": 2,
+        "title": "3-tier Architecture",
+        "thumbnail": img3,
+        "img": img3,
+        "liveDemo": img3,
+        "description": "Scalable Three-tier Web Application Architecture on AWS",
+        "tools": [
+            "ec2",
+            "vpc",
+            "alb",
+            "rds",
+            "autoscaling group"
+        ],
+        "url": "three-tier-web-app",
+        "blogLink": null,
+        "githubLink": "https://github.com/ella-adeka/ThreeTierArchitecturewithAWS",
+        "liveSite": null,
+        "comingSoon": false
+    },
+    {
+        "id": 3,
+        "title": "DOPH",
+        "thumbnail": img3,
+        "img": img3,
+        "liveDemo": img3,
+        "description": "An open-source DevOps Projects Hub open to all",
+        "tools": [
+            "git",
+            "github actions",
+            "mkdocs",
+            "python"
+        ],
+        "url": "doph",
+        "blogLink": null,
+        "githubLink": "https://github.com/ella-adeka/DOPH.git",
+        "liveSite": null,
+        "comingSoon": true
+    },
+    {
+        "id": 4,
+        "title": "QR Code Generator",
+        "thumbnail": img3,
+        "img": img3,
+        "liveDemo": img3,
+        "description": "An app that converts a URL to a QR Code",
+        "tools": [
+            "github actions",
+            "nextjs",
+            "python",
+            "docker",
+            "cloudformation",
+            "prometheus",
+            "grafana",
+        ],
+        "url": "qr-code-generator",
+        "blogLink": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance",
+        "githubLink": "https://github.com/ella-adeka/qr-code-generator",
+        "liveSite": null,
+        "comingSoon": true
+    },
+    // {
+    //     "id": 5,
+    //     "title": "Jectam",
+    //     "thumbnail": img2,
+    //     "img": img2,
+    //     "liveDemo": img2,
+    //     "description": "An integrated automated project management system",
+    //     "tools": [
+    //         "aws",
+    //         "terraform"
+    //     ],
+    //     "url": "jectam",
+    //     "blogLink": "https://ellaadeka.hashnode.dev/deploying-a-flask-application-from-github-on-an-ec2-instance",
+    //     "githubLink": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+    //     "liveSite": "https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git",
+    //     "comingSoon": true
+    // }
+]
+
+export default data;
